393b6449fe77cf21e5de0f48a3be891967e35ea7
Kseniya Platonava <ksu2008@gmail.com>
2019-02-09 13:28:49 -0500

    Create Action Get

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,12 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers';
+import rootReducer from './reducers';
 
-const store = createStore(
-  reducer, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(thunk, logger)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger)
+)
 
 ReactDOM.render(
   <Provider store={store}>
