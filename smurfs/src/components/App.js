4f6f40e283401bffef25559b5dc9830e0a62b5a7
Kseniya Platonava <ksu2008@gmail.com>
2019-02-09 14:10:06 -0500

    Add Smurf, Smurfs, Got it Displayed

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//import { connect } from "react-redux";
+
 import SmurfList from './SmurfList';
 import SmurfForm from './SmurfForm';
 
